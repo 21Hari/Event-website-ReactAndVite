feca677768c973ef183e8024699c348d492722bf
hari <haribalaji838@gmail.com>
2025-06-09 17:38:03 +0530

    Fixed env key bug and updated API integration

--- src/Redux/middleware/RecommentedEventMiddleware.js
+++ src/Redux/middleware/RecommentedEventMiddleware.js
@@ -3,24 +3,28 @@ import EventSlice from "../EventSlice";
 import upcomingEventElement from "../UpcomingEventSlice";
 
 
-const BASE_URL = import.meta.env.VITE_API_BASE
-console.log(BASE_URL)
-const API_KEY = import.meta.env.VITE_API_CODE
+const BASE_URL = import.meta.env.VITE_API_BASE;
+const API_KEY = import.meta.env.VITE_API_CODE;
 
-const BASE_URL2 = import.meta.env.VITE_API_BASE2
-const API_KEY2 = import.meta.env.VITE_API_CODE2
+const BASE_URL2 = import.meta.env.VITE_API_BASE2;
+const API_KEY2 = import.meta.env.VITE_API_CODE2;
 
 const action = EventSlice.actions;
 
 const actionEle = upcomingEventElement.actions;
 
 export const fetchRecommentMiddleware = async (dispatch) => {
+    console.log("fetchUpcomingMiddleware is starting")
     try {
         dispatch(action.userLoading())
         const resp = await fetch(`${BASE_URL}?code=${API_KEY}&type=reco`);
 
+
         const user = await resp.json();
         //console.log(user)
+        //console.log(resp.status)
+        //console.log(resp.body)
+
 
         dispatch(action.recommentedData(user));
 
@@ -34,8 +38,10 @@ export const fetchUpcomingMiddleware = async (dispatch) => {
     try {
         dispatch(actionEle.userLoading())
         const resp = await fetch(`${BASE_URL2}?code=${API_KEY2}&page=1&type=upcoming`)
+
         const data = await resp.json()
-        //console.log(data)
+        console.log(data)
+        console.log(resp.status)
         dispatch(actionEle.upcomingEventData(data))
         console.log("Data fetched Successfully") //checking middleware working
     } catch (err) {
